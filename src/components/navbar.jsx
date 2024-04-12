ebaf9871e29ce6c3d3c860fd230f2b1f23f5a6c3
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-12 15:16:00 +0200

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,12 @@
 // Imports the React library, which is necessary for creating React components
 import React from "react"; 
+
 // Imports the Link component from the react-router-dom library, which is used for navigation
 import { Link } from "react-router-dom"; 
+
 // Imports the ShoppingCart icon component from the phosphor-react library
 import { ShoppingCart } from "phosphor-react"; 
+
 // Imports the styles from the "navbar.css" file located in the same directory as this component
 import "./navbar.css"; 
 
@@ -17,7 +20,7 @@ export const Navbar = () => {
 
         <Link to="/"> Shop </Link> {/* Renders a Link component that navigates to the root ("/") route when clicked, and displays the text "Shop" */}
         <Link to="/contact"> Contact </Link> {/* Renders a Link component that navigates to the "/contact" route when clicked, and displays the text "Contact" */}
-        <Link to="/cart"> <ShoppingCart size={32} /> </Link> {/* Renders a Link component that navigates to the "/cart" route when clicked, and renders a ShoppingCart icon component with a size of 32 */}
+        <Link to="/cart"> <ShoppingCart size={30} /> </Link> {/* Renders a Link component that navigates to the "/cart" route when clicked, and renders a ShoppingCart icon component with a size of 32 */}
      
       </div>
 
